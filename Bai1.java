dded8731747e04aadbddb744dd975353e78ee09d
ngohuuduong <duongakvip1@gmail.com>
2020-03-26 16:40:57 +0700

    Thuat toan 1

---
+++ Bai1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        do {
+            ArrayList<Integer> numbers = new ArrayList<>();
+            int m = n;
+            for(int i = 0; i <=n; i++){
+                for(int j = 2; j <=n; j++){
+                    if(m%j == 0 ){
+                        numbers.add(j);
+                        m = m/j;
+                        break;
+                    }
+                }
+                if(m==1){
+                    break;
+                }
+            }
+            int sum = 0;
+            for(Integer number : numbers){
+                sum += number;
+            }
+            if(sum == n){
+                n = sum;
+                break;
+            } else {
+                n = sum;
+            }
+        } while(true);
+        System.out.println("n = " + n);
+    }
+}
