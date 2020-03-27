48ee15cf3ea963c7642abbe9e17ec5fbb33735cb
ngohuuduong <duongakvip1@gmail.com>
2020-03-27 09:39:23 +0700

    bai2 bai 3

---
+++ Bai2.java
@@ -0,0 +1,43 @@
+/*GCPD (Greatest Common Prime Divisor) được định nghĩa là số nguyên tố lớn nhất
+        là ước của các số nguyên dương cho trước. Nhiệm vụ của bạn là tìm GCPD
+        của hai số nguyên a và b.
+
+        Ví dụ
+
+        Với a = 12 và b = 18, đầu ra là greatestCommonPrimeDivisor(a, b) = 3;
+        Với a = 12 và b = 13, đầu ra là greatestCommonPrimeDivisor(a, b) = -1.*/
+
+import java.util.ArrayList;
+
+public class Bai2 {
+    public static int UCLN(int a, int b){
+        int temp;
+        while (b != 0){
+            temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+    public static int isPrime(int number){
+        if(number == 1) {
+            return -1;
+        }
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int j = 0; j <= number; j++ ) {
+            for (int i = 2; i <= number; i++) {
+                if(number % i == 0) {
+                    numbers.add(i);
+                    number = number/i;
+                    break;
+                }
+            }
+        }
+        int resuilt = numbers.get(numbers.size() - 1);
+        return resuilt;
+    }
+    public static void main(String[] args) {
+        int GCPD = UCLN(12, 13);
+        System.out.println(isPrime(GCPD));
+    }
+}
---
+++ Bai3.java
@@ -0,0 +1,24 @@
+public class Bai3 {
+    public static  int maxFraction(int[] numerators, int[] denominators) {
+        float[] resuilt = new float[numerators.length];
+        for(int i = 0; i < numerators.length ; i++){
+            float x =  numerators[i];
+            float y = denominators[i];
+            resuilt[i] = x/y;
+        }
+        float max = resuilt[0];
+        int index = 0;
+        for(int i = 1; i<resuilt.length; i++ ){
+            if(max < resuilt[i]){
+                max = resuilt[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[] a = {1,2};
+        int[] b = {4,3};
+        System.out.println(maxFraction(a, b));
+    }
+}
