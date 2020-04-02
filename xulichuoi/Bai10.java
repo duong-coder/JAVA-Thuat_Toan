b61388e1c20b19ed2e04c9bbee0fd8faa29c9675
ngohuuduong <duongakvip1@gmail.com>
2020-04-02 09:16:31 +0700

    Xu li chuoi bai 10 11

---
+++ xulichuoi/Bai10.java
@@ -0,0 +1,48 @@
+//Cho một xâu kí tự s gồm nhiều từ. Trong đó mỗi từ sẽ được bắt đầu bởi một kí tự in hoa.
+//
+//        Vì một lí do gì đó, người ta quên chèn các khoảng trắng giữa các từ.
+//
+//        Hãy viết chương trình sửa lại xâu kí tự đó theo yêu cầu sau:
+//
+//        Tách các từ, và thêm 1 kí tự cách (space) giữa 2 từ liên tiếp
+//        Chuyển đổi các kí tự in hoa thành kí tự in thường
+//        Ví dụ
+//
+//        Với s = "CodesignalIsAwesome", thì kết quả amendTheSentence(s) = "codesignal is awesome"
+//        Với s = "Hello", thì kết quả amendTheSentence(s) = "hello".
+//        Đầu vào/Đầu ra:
+//
+//        [Thời gian chạy] 0.5 giây với C++, 3s với Java và C#, 4s với Python, JS và GO
+//
+//        [Đầu vào] string s
+//        Một xâu chứa kí tự chữ cái in hoa và in thường.
+//        Điều kiện:
+//        3 ≤ s.length ≤ 100.
+//
+//        [Đầu ra] string
+//        Câu đã thay đổi.
+
+package xulichuoi;
+
+import java.util.Arrays;
+
+public class Bai10 {
+    static String amendTheSentence(String s) {
+        String[] kiTu = s.split("");
+        String resuilt = "";
+        for (int i = 0; i<s.length(); i++){
+            if(kiTu[i].toUpperCase() == kiTu[i]){
+                kiTu[i] = " " + kiTu[i];
+            }
+            resuilt += kiTu[i];
+        }
+        return resuilt.toLowerCase();
+    }
+    public static void main(String[] args) {
+//        String s = "NgoHuuDuong";
+//        String str = "N";
+//        boolean kq = str.toUpperCase() == str;
+//        System.out.println(kq +" "+ str);
+        System.out.println(amendTheSentence("NgoHuuDuong"));
+    }
+}
---
+++ xulichuoi/Bai11XauPalindrome.java
@@ -0,0 +1,40 @@
+//Một xâu được gọi là palindrome nếu viết xuôi hay viết ngược xâu đó đều cho ra kết quả giống nhau
+//
+//        Cho một xâu kí tự, kiểm tra nó có phải xâu palindrome không.
+//
+//        Ví dụ
+//
+//        Với inputString = "aabaa", kết quả checkPalindrome(inputString) = true;
+//        Với inputString = "abac", kết quả checkPalindrome(inputString) = false;
+//        Với inputString = "a", kết quả checkPalindrome(inputString) = true.
+//        Đầu vào/Đầu ra
+//
+//        [Thời gian chạy] 0.5 giây
+//
+//        [Đầu vào] string inputString.
+//        Xâu không rỗng chứa các kí tự chữ cái in thường
+//        Điều kiện:
+//        1 ≤ inputString.length ≤ 105.
+//
+//        [Đầu ra] boolean
+//
+//        true nếu inputString là xâu palindrome, false nếu không.
+
+package xulichuoi;
+
+public class Bai11XauPalindrome {
+    static boolean checkPalindrome(String inputString) {
+        int n = inputString.length();
+        String[] str = inputString.split("");
+        for(int i = 0; i <= n/2; i++){
+            if(!str[i].equals(str[n - i - 1])){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(checkPalindrome("aabaa"));
+        System.out.println(checkPalindrome("aaba"));
+    }
+}
